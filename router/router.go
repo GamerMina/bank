6ebc5a9049280845a4352509ca05c87f7207e971
GamerMina <qiwiqiwi123456@gmail.com>
2026-02-14 15:45:37 +0500

    maked privite

--- router/router.go
+++ router/router.go
@@ -1,1 +1,13 @@
 package router
+
+import (
+	"bank/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.Engine, H *handler.Handler) {
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, "pong")
+	})
+
+}
